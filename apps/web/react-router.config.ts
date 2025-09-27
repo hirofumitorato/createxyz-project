98cdf66faa638935ad36d5c7bd7f297fe4513185
HIROFUMI TORATO <hirofumitorato@gmail.com>
2025-09-28 07:12:15 +0900

    Remove buildEnd hook from React Router config

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -4,8 +4,4 @@ export default {
 	appDirectory: './src/app',
 	ssr: true,
 	prerender: ['/*?'],
-	buildEnd: async ({ viteConfig }) => {
-		viteConfig.build = viteConfig.build || {};
-		viteConfig.build.target = 'es2022';
-	},
 } satisfies Config;
