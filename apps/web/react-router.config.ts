1fc0e45d2d25b6ce1f2d331d67b1c5b86dfb9d85
HIROFUMI TORATO <hirofumitorato@gmail.com>
2025-09-28 02:48:44 +0900

    Fix build target for top-level await support

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -4,4 +4,8 @@ export default {
 	appDirectory: './src/app',
 	ssr: true,
 	prerender: ['/*?'],
+	buildEnd: async ({ viteConfig }) => {
+		viteConfig.build = viteConfig.build || {};
+		viteConfig.build.target = 'es2022';
+	},
 } satisfies Config;
